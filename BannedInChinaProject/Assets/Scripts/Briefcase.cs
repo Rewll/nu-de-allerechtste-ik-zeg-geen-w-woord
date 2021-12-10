b9761db00b19d5f57aa2f48bce78556a99abbfc0
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2021-12-10 23:06:53 +0100

    Added Prefabs

--- BannedInChinaProject/Assets/Scripts/Briefcase.cs
+++ BannedInChinaProject/Assets/Scripts/Briefcase.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class Briefcase : MonoBehaviour
 {
+    private PersonGenration PersonScript;
+    private GameManager GameManager;
+    [SerializeField] private GameObject gameManager;
+    [SerializeField] private GameObject people;
+    [SerializeField] private int AmountOfItems; 
 
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        PersonScript = people.GetComponent<PersonGenration>();
+        GameManager = gameManager.GetComponent<GameManager>();
+    }
+    public void GenerateNewBriefcase()
+    {
+        if (PersonScript.personStateOfLegal.ToString() == "illegalSuitCase")
+        {
+            Instantiate(GameManager.AlleIllegalIitems[Random.Range(0, GameManager.AlleIllegalIitems.Count)]);
+            for (int i = 0; i < AmountOfItems -1; i++)
+            {
+                Instantiate(GameManager.AlleLegalItems[Random.Range(0, GameManager.AlleLegalItems.Count)]);
+            }
+        }
+        else
+        {
+
+        }
+
+
     }
 }
