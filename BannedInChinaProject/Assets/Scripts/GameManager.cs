34a80478a938b01a999357395cb8bdd15f2ec3df
Raoul Scholtens <Raoulscholtens@gmail.com>
2021-12-10 22:19:14 +0100

    control s

--- BannedInChinaProject/Assets/Scripts/GameManager.cs
+++ BannedInChinaProject/Assets/Scripts/GameManager.cs
@@ -7,15 +7,14 @@ public class GameManager : MonoBehaviour
     //public List<GameObject> Personen = new List<GameObject>();
 
     public int mensenAantal;
-    public List<string> namenLijst = new List<string>();
-
+    [Space]
+    public List<string> gekozenNamenLijst = new List<string>();
+    [Space]
     public List<string> alleVoorNamen = new List<string>();
     public List<string> alleAchterNamen = new List<string>();
     public List<string> alleBeroepen = new List<string>();
-
-
-    public List<GameObject> legalItems = new List<GameObject>();
-    public List<GameObject> illegalItems = new List<GameObject>();
+    public List<GameObject> AlleIllegalIitems = new List<GameObject>();
+    public List<GameObject> AlleLegalItems = new List<GameObject>();
 
 
     void Start()
@@ -25,13 +24,13 @@ public class GameManager : MonoBehaviour
     //wist je da thg
     void genereerLijst()
     {
-        while (namenLijst.Count != mensenAantal)
+        while (gekozenNamenLijst.Count != mensenAantal)
         {
             string name = alleVoorNamen[Random.Range(0, alleVoorNamen.Count)] + " " +
               alleAchterNamen[Random.Range(0, alleAchterNamen.Count)];
-            if (!namenLijst.Contains(name))
+            if (!gekozenNamenLijst.Contains(name))
             {
-                namenLijst.Add(name);
+                gekozenNamenLijst.Add(name);
             } 
         }
     }
