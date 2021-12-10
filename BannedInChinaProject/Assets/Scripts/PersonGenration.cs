ebc6ab0a4c5bed71971f3d7c423b02c9c2125c3d
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2021-12-10 22:03:44 +0100

    Start with some scripts

---
+++ BannedInChinaProject/Assets/Scripts/PersonGenration.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonGenration : MonoBehaviour
+{
+    public string PersonName;
+    public string Job;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
